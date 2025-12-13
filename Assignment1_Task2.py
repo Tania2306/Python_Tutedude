b2cbb06507070fb1ff7d51de1fa5892d8193b8b6
Tania Robby <taniarobby.2306@gmail.com>
2025-12-13 19:34:57 +0530

    Update Assignment1_Task2.py

--- Assignment1_Task2.py
+++ Assignment1_Task2.py
@@ -1,3 +1,4 @@
 str1 = input("Enter your first name: ")
 str2 = input("Enter your last name: ")
-print("Hello,",str1,str2,"Welcome to the Python Program.")
+
+print("Hello,",str1+" "+str2,"! Welcome to the Python Program.")
