b622075c3b4dd8649173f23d6da9b540931c366c
Tania Robby <taniarobby.2306@gmail.com>
2025-12-16 14:29:22 +0530

    Add files via upload

---
+++ Assignment3_Task2.py
@@ -0,0 +1,5 @@
+import math
+num = float(input("Enter a number: "))
+print("Square root:", math.sqrt(num))
+print("Logarithm (log base e): ",math.log(num))
+print("Sine (in radians): ", math.sin(num))
